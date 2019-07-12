f053417de920d9fa96e67c534fbed5257a2ec829
jasonwj96 <jasonwj96@gmail.com>
2019-07-11 21:29:06 -0500

    Fixed a breadcrumb bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.scss";
-// import Navbar from "./components/navbar";
 import Menu from "./components/menu";
 import Breadcrumbs from "./components/breadcrumbs";
 
 const App = () => {
   return (
     <div className="App">
-      {/* <Navbar></Navbar> */}
       <Menu />
       <Breadcrumbs />
     </div>
--- src/components/breadcrumbs.jsx
+++ src/components/breadcrumbs.jsx
@@ -3,19 +3,40 @@ import "./breadcrumbs.scss";
 
 const Breadcrumbs = props => {
   const separator = ">";
-  const items = ["Página principal", separator, "Eventos"];
+  const items = [
+    "Página principal",
+    separator,
+    "Eventos",
+    separator,
+    "2019",
+    separator,
+    "Julio",
+    separator,
+    "Feria de empleo"
+  ];
 
   return (
     <div className="breadcrumbs-container">
       <div className="items">
         {items.map((item, i) => {
-          return item === separator ? (
-            <i key={i} className="fas fa-chevron-right separator" />
-          ) : (
-            <a key={i} href={"/" + item} className="bc-item link">
-              {item}
-            </a>
-          );
+          if (item === separator)
+            return <i key={i} className="fas fa-chevron-right separator" />;
+          else {
+            let itemRoute = "/";
+
+            for (let j = 0; j <= i; j++) {
+              //skip the separators
+              if (!(j % 2 === 1)) {
+                itemRoute += items[j] + "/";
+              }
+            }
+
+            return (
+              <a key={i} href={itemRoute} className="bc-item link">
+                {item}
+              </a>
+            );
+          }
         })}
       </div>
     </div>
