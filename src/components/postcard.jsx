5d3345f8ae4ef967602c557a04e30bbbde15260a
jasonwj96 <jasonwj96@gmail.com>
2019-07-12 23:34:02 -0500

    Added a message to postcard component

--- src/components/postcard.jsx
+++ src/components/postcard.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./postcard.scss";
 
-const Postcard = () => {
+const Postcard = props => {
+  const [userEmail, setUserEmail] = useState("");
+
+  const handleEmailChange = event => {
+    setUserEmail(event.target.value);
+  };
+
+  useEffect(() => {}, [userEmail]);
+
   return (
     <div className="postcard-container">
+      <p className="postcard-msg">
+        Desea obtener más información en su correo? mueva el ratón encima de la
+        carta
+      </p>
       <div className="mail">
         <div className="cover" />
         <div className="letter">
-          {/* <input type="text" />
-           */}
-          <h1>@</h1>
+          <p>Introduzca su correo:</p>
+          <form onSubmit={e => e.preventDefault()}>
+            <input
+              id="emailInput"
+              type="email"
+              name="userEmail"
+              placeholder="brucewayne@gmail.com"
+              value={userEmail}
+              onChange={handleEmailChange}
+              onClick={handleEmailChange}
+            />
+          </form>
         </div>
       </div>
     </div>
