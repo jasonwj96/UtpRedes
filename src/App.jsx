004601220567c9e4fcce6e606a450f6c4009b022
jkmr2596 <jkmr112526@outlook.es>
2019-07-14 15:09:06 -0600

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ import Menu from "./components/menu";
 import Breadcrumbs from "./components/breadcrumbs";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "./containers/home";
-<<<<<<< HEAD
-import Materias from "./containers/materias"; 
-=======
 import Materias from "./containers/materias";
->>>>>>> ae8110df50e61db5ef6bee6d7b5f7982b9c69979
 // import Postcard from "./components/postcard";
 
 const App = () => {
@@ -19,17 +15,9 @@ const App = () => {
         <Breadcrumbs />
         <Redirect from="/" to="/inicio" />
         <Route path="/inicio" component={Home} />
-<<<<<<< HEAD
-         {/* <--- ahi puse una nota */}    
-         <Route path="/inicio/materias" component={Materias} />
-        {/* <Postcard /> */}
-      </Router>
-
-=======
         <Route path="/materias" component={Materias} />
         {/* <Postcard /> */}
       </Router>
->>>>>>> ae8110df50e61db5ef6bee6d7b5f7982b9c69979
     </div>
   );
 };
