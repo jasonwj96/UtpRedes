549068299fe63afa1329271cac5915091f0220df
jkmr2596 <jkmr112526@outlook.es>
2019-07-14 09:58:45 -0600

    cambios2

--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -22,12 +22,8 @@ const Home = props => {
             <Link to="/inicio">Inicio</Link>
             <Link to="/inicio/eventos">Inicio/Eventos</Link>
             <Link to="/inicio/eventos/2019">Inicio/Eventos/2019</Link>
-            <Link to="/inicio/eventos/2019/julio">
-              Inicio/Eventos/2019/julio
-            </Link>
-            <Link to="/inicio/eventos/2019/julio/feria de empleo">
-              Inicio/Eventos/2019/julio/feria de empleo
-            </Link>
+            <Link to="/inicio/eventos/2019/julio">Inicio/Eventos/2019/julio</Link>
+            <Link to="/inicio/eventos/2019/julio/feria de empleo">Inicio/Eventos/2019/julio/feria de empleo</Link>
           </div>
 
           <div className="welcome-footer">
