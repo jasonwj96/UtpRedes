7743b7bdacb5a4ddd4f1fb27e0a9e23855898bb5
jkmr2596 <jkmr112526@outlook.es>
2019-07-14 11:12:27 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Menu from "./components/menu";
 import Breadcrumbs from "./components/breadcrumbs";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "./containers/home";
-import Home from "./containers/materias";
+import Materias from "./containers/materias";
 
 // import Postcard from "./components/postcard";
 
