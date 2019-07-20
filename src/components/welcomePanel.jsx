aba9f384b85b0634b3111ff5098847970134b34b
jasonwj96 <jasonwj96@gmail.com>
2019-07-20 18:08:20 -0500

    Added welcomePanel component

---
+++ src/components/welcomePanel.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./welcomePanel.scss";
+
+const WelcomePanel = () => {
+  return (
+    <div className="welcome-container">
+      <iframe
+        title="UTP Panama Intro"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/oznPAZNJu9o"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+      <div className="welcome-info">
+        <p className="welcome-title">Licenciatura en redes informáticas</p>
+
+        <div className="welcome-footer">
+          <div className="icon">
+            <i className="fas fa-clock" />
+            <p>
+              Duración: <br /> 4 años
+            </p>
+          </div>
+          <div className="icon">
+            <i className="fas fa-building" />
+            <p>Gran oportunidad laboral</p>
+          </div>
+          <div className="icon">
+            <i className="fas fa-graduation-cap" />
+            <p>1000+ egresados</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePanel;
--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -1,41 +1,12 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import "./home.scss";
+import WelcomePanel from "../components/welcomePanel";
 
 const Home = props => {
   return (
     <div className="home-container">
-      <div className="welcome-container">
-        <iframe
-          title="UTP Panama Intro"
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/oznPAZNJu9o"
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-        <div className="welcome-info">
-          <p className="welcome-title">Licenciatura en redes informáticas</p>
-         
-          <div className="welcome-footer">
-            <div className="icon">
-              <i className="fas fa-clock" />
-              <p>
-                Duración: <br /> 4 años
-              </p>
-            </div>
-            <div className="icon">
-              <i className="fas fa-building" />
-              <p>Gran oportunidad laboral</p>
-            </div>
-            <div className="icon">
-              <i className="fas fa-graduation-cap" />
-              <p>1000+ egresados</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <WelcomePanel />
     </div>
   );
 };
