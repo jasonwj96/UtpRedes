b3f69f443c3468a84671dc9d41f253f33de1a30c
jasonwj96 <jasonwj96@gmail.com>
2019-07-30 15:59:51 -0500

    Added images

--- src/components/aboutItem.jsx
+++ src/components/aboutItem.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "./aboutItem.scss";
-import slide1 from "../img/slide2.jpg";
 
-const AboutItem = () => {
+const AboutItem = props => {
   const [className, setClassName] = useState("aboutItem-container");
   const [toggleItem, setToggleItem] = useState(false);
+  const images = require.context("../img/assets/", true);
 
   const toggle = () => {
     if (toggleItem) setClassName("aboutItem-container expand");
@@ -13,31 +13,20 @@ const AboutItem = () => {
     setToggleItem(!toggleItem);
   };
 
+  const { imageUrl, overlayText, desc } = props;
+
   return (
     <div className={className} onClick={toggle}>
       <div className="overlay">
-        <i class="fas fa-university" />
-        <p className="overlay-text">Instalaciones</p>
+        <i className="fas fa-university" />
+        <p className="overlay-text">{overlayText}</p>
       </div>
-      <img src={slide1} alt="" />
+      <img src={images(`${imageUrl}`)} alt="" />
       <div className="info">
         <button className="close-btn" onClick={toggle}>
           <i className="fas fa-times" />
         </button>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
-          vitae dolorum nisi? Id similique molestias facilis asperiores tempore
-          itaque, neque quia aliquid quos doloribus unde, odit sed animi!
-          Facilis esse numquam ut? Consequuntur distinctio sed, totam eum
-          assumenda ab nobis ipsa quisquam corporis fugiat qui explicabo
-          cupiditate doloremque expedita cumque eligendi fugit necessitatibus
-          et. Eveniet dolores voluptatibus repellendus animi ipsum nostrum
-          ipsam! Tenetur neque quod rem voluptatem suscipit? Nostrum iusto
-          culpa, ad ab voluptatem, accusantium illo ratione molestias iure
-          consequuntur sed, exercitationem sit. Accusamus eveniet necessitatibus
-          numquam inventore quidem, quae recusandae reiciendis vero sit
-          perspiciatis tempore officiis nam, minus quis.
-        </p>
+        <p>{desc}</p>
       </div>
     </div>
   );
--- src/containers/acerca.jsx
+++ src/containers/acerca.jsx
@@ -2,17 +2,41 @@ import React from "react";
 import "./acerca.scss";
 import AboutItem from "../components/aboutItem";
 
-const Acerca = () => {
+const Acerca = props => {
+  const info = [
+    {
+      imageUrl: "./slide1.jpg",
+      desc:
+        "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero ea, sapiente similique delectus consequatur architecto aspernatur quia, beatae hic nisi fugiat, voluptas impedit? Reiciendis iusto beatae aliquam esse quam debitis ex et atque est perspiciatis reprehenderit nihil ullam earum exercitationem ipsa facere quasi nesciunt, cupiditate expedita neque minus eum! Assumenda placeat alias, hic eligendi voluptas voluptatum, reiciendis in porro facilis nostrum temporibus repellat perferendis unde! Quisquam quos voluptates maxime rerum voluptatibus minima veritatis dolores, adipisci molestiae tenetur sed, libero ea ullam officiis. Voluptatum sit, unde corporis a nobis, veritatis sed ipsum vero iusto sapiente illum. Repellat aperiam architecto vero est?",
+      overlayText: "Instalaciones"
+    },
+    {
+      imageUrl: "./slide2.jpg",
+      desc:
+        "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero ea, sapiente similique delectus consequatur architecto aspernatur quia, beatae hic nisi fugiat, voluptas impedit? Reiciendis iusto beatae aliquam esse quam debitis ex et atque est perspiciatis reprehenderit nihil ullam earum exercitationem ipsa facere quasi nesciunt, cupiditate expedita neque minus eum! Assumenda placeat alias, hic eligendi voluptas voluptatum, reiciendis in porro facilis nostrum temporibus repellat perferendis unde! Quisquam quos voluptates maxime rerum voluptatibus minima veritatis dolores, adipisci molestiae tenetur sed, libero ea ullam officiis. Voluptatum sit, unde corporis a nobis, veritatis sed ipsum vero iusto sapiente illum. Repellat aperiam architecto vero est?",
+      overlayText: "Oportunidad laboral"
+    },
+    {
+      imageUrl: "./slide3.jpg",
+      desc:
+        "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero ea, sapiente similique delectus consequatur architecto aspernatur quia, beatae hic nisi fugiat, voluptas impedit? Reiciendis iusto beatae aliquam esse quam debitis ex et atque est perspiciatis reprehenderit nihil ullam earum exercitationem ipsa facere quasi nesciunt, cupiditate expedita neque minus eum! Assumenda placeat alias, hic eligendi voluptas voluptatum, reiciendis in porro facilis nostrum temporibus repellat perferendis unde! Quisquam quos voluptates maxime rerum voluptatibus minima veritatis dolores, adipisci molestiae tenetur sed, libero ea ullam officiis. Voluptatum sit, unde corporis a nobis, veritatis sed ipsum vero iusto sapiente illum. Repellat aperiam architecto vero est?",
+      overlayText: "Reconocimiento internacional"
+    }
+  ];
+
   return (
     <div className="acerca-container">
-      {/* <p className="title">Puede pulsar en cualquier elemento para saber más</p> */}
       <div className="grid">
-        <AboutItem />
-        <AboutItem />
-        <AboutItem />
-        <AboutItem />
-        <AboutItem />
-        <AboutItem />
+        {info.map((item, index) => {
+          return (
+            <AboutItem
+              id={index}
+              imageUrl={item.imageUrl}
+              desc={item.desc}
+              overlayText={item.overlayText}
+            />
+          );
+        })}
       </div>
     </div>
   );
