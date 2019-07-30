f49f453bd5db3333e2fb87e39b2636471e28264b
jasonwj96 <jasonwj96@gmail.com>
2019-07-30 16:33:09 -0500

    Added icons

--- src/components/aboutItem.jsx
+++ src/components/aboutItem.jsx
@@ -5,6 +5,7 @@ const AboutItem = props => {
   const [className, setClassName] = useState("aboutItem-container");
   const [toggleItem, setToggleItem] = useState(false);
   const images = require.context("../img/assets/", true);
+  const { imageUrl, overlayText, desc } = props;
 
   const toggle = () => {
     if (toggleItem) setClassName("aboutItem-container expand");
@@ -13,12 +14,24 @@ const AboutItem = props => {
     setToggleItem(!toggleItem);
   };
 
-  const { imageUrl, overlayText, desc } = props;
+  const overlayIcon = () => {
+    if (overlayText === "Instalaciones") {
+      return <i className="fas fa-university" />;
+    } else if (overlayText === "Oportunidad laboral") {
+      return <i className="fas fa-briefcase" />;
+    } else if (overlayText === "Reconocimiento internacional") {
+      return <i className="fas fa-globe-americas" />;
+    } else if (overlayText === "Cuerpo docente") {
+      return <i className="fas fa-chalkboard-teacher" />;
+    } else if (overlayText === "Egresados") {
+      return <i className="fas fa-user-graduate" />;
+    }
+  };
 
   return (
     <div className={className} onClick={toggle}>
       <div className="overlay">
-        <i className="fas fa-university" />
+        {overlayIcon()}
         <p className="overlay-text">{overlayText}</p>
       </div>
       <img src={images(`${imageUrl}`)} alt="" />
