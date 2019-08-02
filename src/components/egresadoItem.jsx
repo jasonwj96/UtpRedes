8a6d768d76174278f58cafa2aad2c148e962dc0d
jasonwj96 <jasonwj96@gmail.com>
2019-08-01 21:55:53 -0500

    Added components

---
+++ src/components/egresadoItem.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "./egresadoItem.scss";
+
+const EgresadoItem = props => {
+  const [className, setClassName] = useState("egresadoItem-container");
+  const [toggleItem, setToggleItem] = useState(false);
+  const images = require.context("../img/assets/", true);
+  const { imageUrl, overlayText, desc } = props;
+
+  useEffect(() => {
+    if (toggleItem) setClassName("egresadoItem-container expand");
+    else setClassName("egresadoItem-container");
+  }, [toggleItem]);
+
+  const toggle = () => setToggleItem(!toggleItem);
+
+  return (
+    <div className={className} onClick={toggle}>
+      <div className="overlay">
+        <p className="overlay-text">{overlayText}</p>
+      </div>
+      <img src={images(`${imageUrl}`)} alt="" />
+      <div className="info">
+        <button className="close-btn" onClick={toggle}>
+          <i className="fas fa-times" />
+        </button>
+        <img src={images(`./alejandro.jpg`)} alt="alejandro" />
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default EgresadoItem;
--- src/containers/perfilEgresado.jsx
+++ src/containers/perfilEgresado.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import "./acerca.scss";
-import AboutItem from "../components/aboutItem";
+import "./perfilEgresado.scss";
+import EgresadoItem from "../components/egresadoItem";
 
 const perfilEgresado = props => {
   const info = [
     {
       imageUrl: "./entrevistas.jpg",
       desc:
-      <p align="justify">La carrera de Administración de Redes y Comunicaciones es una profesión muy solicitada en todos los rubros de nuestro mercado, pero ante esa necesidad, el nivel de especialización y capacitación es muy exigente; es por ello, que anualmente realizamos un Comité Técnico Consultivo con representantes de empresas líderes en distintos rubros para que los miembros puedan proporcionarle a nuestros alumnos asesoría técnica y de esa manera conozcan lo que se requiere o se necesita hoy en día en nuestro país</p>,
+        "La carrera de Administración de Redes y Comunicaciones es una profesión muy solicitada en todos los rubros de nuestro mercado, pero ante esa necesidad, el nivel de especialización y capacitación es muy exigente; es por ello, que anualmente realizamos un Comité Técnico Consultivo con representantes de empresas líderes en distintos rubros para que los miembros puedan proporcionarle a nuestros alumnos asesoría técnica y de esa manera conozcan lo que se requiere o se necesita hoy en día en nuestro país",
+
       overlayText: "Entrevistas"
     },
     {
@@ -21,15 +22,15 @@ const perfilEgresado = props => {
       desc:
         "La Universidad Tecnológica de Panamá es la única universidad panameña en el QS World University Rankings, manteniéndose en el rango de 801 a 1,000 universidades. En los dos últimos años la UTP ha recibido a 320 estudiantes de Europa, América Latina y Asia y ha enviado a más de 280 estudiantes a través de los Programas de Internacionalización estudiantil que desarrolla esta institución.",
       overlayText: "Recomendaciones"
-    },
+    }
   ];
 
   return (
-    <div className="acerca-container">
+    <div className="perfilEgresado-container">
       <div className="grid">
         {info.map((item, index) => {
           return (
-            <AboutItem
+            <EgresadoItem
               key={index}
               id={index}
               imageUrl={item.imageUrl}
