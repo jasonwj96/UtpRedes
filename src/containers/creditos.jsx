cd3a4231d1859e409dd5a0f744a2d047d7a3b4ca
jasonwj96 <jasonwj96@gmail.com>
2019-08-02 02:49:47 -0500

    creditos terminados

--- src/containers/creditos.jsx
+++ src/containers/creditos.jsx
@@ -8,108 +8,62 @@ import "./creditos.scss";
 const creditos = props => {
   return (
     <div className="creditos-container">
-      <div>
-        <br />
-        <br />
-        <table cellspacing="15" cellpadding="5">
-          <tr>
-            <tr>
-              <td />
-              <td />
-              <td />
-              <td />
-              <td />
-            </tr>
-            <td>
-              <img src={JasonA} alt="" width="200" height="200" />
-            </td>
-            <div>
-              <strong>Jason, Arrocha</strong>
-              <br />
-              Encargado de:
-              <ul>
-                <li>StoryBoards</li>
-                <li>Documento</li>
-                <li>Diagramas</li>
-                <li>Pantallas</li>
-                <ul>
-                  <li>Eventos</li>
-                  <li>Perfil Egresados</li>
-                  <li>Créditos</li>
-                </ul>
-              </ul>
-            </div>
-            <td>
-              <img src={JasonW} alt="" width="200" height="200" />
-            </td>
-            <div>
-              <strong>Jason,Wedderburn</strong>
-              <br />
-              Encargado de:
-              <ul>
-                <li>StoryBoards</li>
-                <li>Documento</li>
-                <li>Brindo apoyo sobre la página</li>
-                <li>Pantallas</li>
-                <ul>
-                  <li>Presentación</li>
-                  <li>Acerca de la carrera</li>
-                </ul>
-              </ul>
-            </div>
-          </tr>
-          <tr>
-            <td />
-          </tr>
-          <tr>
-            <td />
-          </tr>
-          <tr>
-            <td />
-          </tr>
-          <tr>
-            <td />
-          </tr>
-          <tr>
-            <td />
-            <td>
-              <img src={JosephM} alt="" width="200" height="200" />
-            </td>
-            <div>
-              <strong>Joseph, Miranda</strong>
-              <br />
-              Encargado de:
-              <ul>
-                <li>StoryBoards</li>
-                <li>Documento</li>
-                <li>Diagramas</li>
-                <li>Pantallas:</li>
-                <ul>
-                  <li>Materias</li>
-                  <li>Ayuda</li>
-                </ul>
-              </ul>
-            </div>
-            <td>
-              <img src={IrvingD} alt="" width="200" height="200" />
-            </td>
-            <div>
-              <strong>Irving, Dominguez</strong>
-              <br />
-              Encargado de:
-              <ul>
-                <li>Encargado de dar las directrices del proyecto</li>
-                <li>StoryBoards</li>
-                <li>Documento</li>
-                <li>Diagramas</li>
-                <li>Pantallas</li>
-                <ul>
-                  <li>Campo Laboral</li>
-                </ul>
-              </ul>
-            </div>
-          </tr>
-        </table>
+      <div className="creditosItem">
+        <img src={JasonA} alt="Jason" />
+        <div className="creditosInfo">
+          <p className="autorName">Jason Arrocha</p>
+          <ul>
+            <li>StoryBoards</li>
+            <li>Documento</li>
+            <li>Diagramas</li>
+            <li>Pantallas</li>
+            <li>Eventos</li>
+            <li>Perfil Egresados</li>
+            <li>Créditos</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="creditosItem">
+        <img src={JasonW} alt="Jason" />
+        <div className="creditosInfo">
+          <p className="autorName">Jason Wedderburn</p>
+          <ul>
+            <li>StoryBoards</li>
+            <li>Documento</li>
+            <li>Soporte frontend</li>
+            <li>Presentación</li>
+            <li>Acerca de la carrera</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="creditosItem">
+        <img src={JosephM} alt="Jason" />
+        <div className="creditosInfo">
+          <p className="autorName">Joseph Miranda</p>
+          <ul>
+            <li>StoryBoards</li>
+            <li>Documento</li>
+            <li>Diagramas</li>
+            <li>Materias</li>
+            <li>Ayuda</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="creditosItem">
+        <img src={IrvingD} alt="Jason" />
+        <div className="creditosInfo">
+          <p className="autorName">Irving Domínguez</p>
+          <ul>
+            <li>Lider del equipo</li>
+            <li>Storyboards</li>
+            <li>Documentos</li>
+            <li>Diagramas</li>
+            <li>Campo laboral</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
